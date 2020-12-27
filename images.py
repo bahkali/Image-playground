792ad91d75f80dc55ad4879bd87313d0daaae53c
kaly <kalimamadou@gmail.com>
2020-12-27 06:42:54 -0800

    Resize the imgae and output on the screen

--- images.py
+++ images.py
@@ -3,4 +3,10 @@ from PIL import Image, ImageFilter
 img = Image.open('./pokedex/pikachu.jpg')
 filtered_img = img.filter(ImageFilter.BLUR)
 filtered_img.save('Blur.png', 'png')
+filtered_img2 = img.convert('L')
+filtered_img2.save('grey.png', 'png')
+# filtered_img2.show()#open the file
+resize = filtered_img2.resize((300, 300))
+resize.save("grey-resize.png", 'png')
+resize.show()
 print('Done')
